ee7625ee9191ab219e54f1dfd3225cba41d4fa5a
renu-renu <102498951+renu-renu@users.noreply.github.com>
2022-05-02 16:53:36 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.233.255.130:9000',
-       options : {
+  serverUrl: 'http://18.223.108.28:9000',
+       options : 
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '219a0309ba48ec3900f263329629970429e31997',
+	    'sonar.login': '37f354d8e11eb0f3b2bd5c8b0d5d0eb61c63f2ad',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
